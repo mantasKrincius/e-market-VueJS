73582d7c32d679f636708caa5ba759946fd9cd65
Mantas <mantaskrincius@gmail.com>
2021-09-07 00:01:54 +0300

    user orders

---
+++ src/components/Profile/OrdersOrdered.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  My Orders:
+    <div v-for="(item, index) in getUserOrder" :key="index">
+      {{item}}
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrdersOrdered",
+  computed: {
+    getUserOrder(){
+      return this.$store.state.userOrders
+    },
+  },
+  mounted() {
+    console.log(this.getUserOrder)
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
