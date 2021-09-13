8377f3a936a39620720bfd42716e15128b21a137
Mantas <mantaskrincius@gmail.com>
2021-09-13 14:16:52 +0300

    favorites

---
+++ src/components/Favourites/Favourites.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex">
+    <div v-for="(item, index) in userFavourites" :key="index">
+      <SinglePost :item="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from "../Home/SinglePost";
+export default {
+  name: "Favourites",
+  components: {SinglePost},
+  computed: {
+    userFavourites(){
+      return this.$store.state.favorites
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
